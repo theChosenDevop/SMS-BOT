e903f3d022aa7431c05f32ed482c46562501b3b1
theChosenDevop <tobbey.adesanya@gmail.com>
2025-05-28 10:12:59 +0100

    chore: add temporary logging for response

--- src/server.ts
+++ src/server.ts
@@ -49,29 +49,32 @@ app.post('/webhook', async (req: Request, res: Response) => {
   const { jsonrpc, method, params, id } = req.body as TelexIntRequest;
   const {message: Message, tone: Message_Tone,recipient: Recipient_Phone_Number, send_time: Send_Time, test_mode: Test_Mode } = params;
   console.log('Received integration data:', Message)
-  console.log(mastra.getLogger().info('Hello from Mastra!'));
+  // console.log(mastra.getLogger().info('Hello from Mastra!'));
   try {
 
-    if (method !== 'contextEnrichment') {
-       res.status(400).json({ error: 'Invalid method' });
-       return;
-    }
-    const agent = await contextEnrichment.generate([{ role: 'user', content: `Reformat this message to be sent as an SMS in a ${Message_Tone} tone: "${Message}"` }])
-    console.log("Agent response: ", agent.text);
-    const summary = agent.text;
+    // if (method !== 'contextEnrichment') {
+    //    res.status(400).json({ error: 'Invalid method' });
+    //    return;
+    // }
+    // const agent = await contextEnrichment.generate([{ role: 'user', content: `Reformat this message to be sent as an SMS in a ${Message_Tone} tone: "${Message}"` }])
+    // console.log("Agent response: ", agent.text);
+    // const summary = agent.text;
 
-    const smsResponse = await sms.send({
-      to: [Recipient_Phone_Number],
-      from: process.env.AFRICASTALKING_SENDER_ID || 'Telex',
-      message: summary,
-    })
+    // const smsResponse = await sms.send({
+    //   to: [Recipient_Phone_Number],
+    //   from: process.env.AFRICASTALKING_SENDER_ID || 'Telex',
+    //   message: summary,
+    // })
 
-    console.log('SMS sent:', smsResponse);
+    // console.log('SMS sent:', smsResponse);
 
-    res.status(200).json({ summary, smsResponse });
+    // res.status(200).json({ summary, smsResponse });
+    res.status(200).json({"message": "work in progress, please check back later", "status": "success"});
+    return;
   } catch (error) {
     console.error("Error in agent generation:", error);
     res.status(500).json({ error: 'Internal server error' });
+    return;
   }
 })
 
